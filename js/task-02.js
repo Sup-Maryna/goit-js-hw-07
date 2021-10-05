6b2424496f23f568b993fd01d6d6737772b03031
Sup-Maryna <1979@ukr.net>
2021-10-05 23:45:40 +0300

    HW7

---
+++ js/task-02.js
@@ -0,0 +1,12 @@
+const ingredients = ["Картошка", "Грибы", "Чеснок", "Помидоры", "Зелень", "Приправы"];
+const container = document.querySelector("ul");
+
+function setIngredientsList() {
+ const listArray = ingredients.map((ingredient) => {
+   const item = document.createElement("li");
+   item.textContent = ingredient;
+   return item;
+ });
+    container.append(...listArray);
+}
+setIngredientsList();
